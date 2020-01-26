1ce78bcbdd2770aa8d0c6c5cf715a7bb4a20a8ae
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-01-26 12:06:42 -0600

    Successfully calling fake json from external source using axios. Still need to figure out a way to call local file

--- react-flashcards/src/App.js
+++ react-flashcards/src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {txt: 'Front Side of Card'}
-    this.update = this.update.bind(this)
-  }
+  state = {
+    cards: [],
+  };
 
-  update( e ) {
-    this.setState({txt: 'Back Side of Card'})
+  // constructor(){
+  //   super();
+  //   this.state = {txt: 'Front Side of Card'}
+  //   this.update = this.update.bind(this)
+  // }
+
+  // update( e ) {
+  //   this.setState({txt: 'Back Side of Card'})
+  // }
+
+  componentDidMount() {
+    // Axios is promise-based
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+      console.log(res)
+      this.setState({ cards: res.data });
+    })
   }
 
   render(){
     return (
       <div className="flipcard-outer">
         <div className="card-content">
-          <h1>{this.state.txt}</h1>
-          <button onClick={this.update}>Test Button</button>
+          <ul>
+              {this.state.cards.map(card => <li>{card.name}</li>)}
+          </ul>
         </div>
       </div>
       

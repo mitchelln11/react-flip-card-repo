c48ac2ec3bef6e4c506cdd7631d3a2e4078f6772
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-01-27 17:16:00 -0600

    restructured project. Console logging local json info. Just need to access it and display it. Can't get Axios to work.

--- react-flashcards/src/App.js
+++ react-flashcards/src/App.js
@@ -1,41 +1,27 @@
-import React from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'; // This has to change from out of the box
+import Cards from './Components/Cards';
 
-class App extends React.Component {
+class App extends Component { // Line updated
+
   state = {
-    cards: [],
-  };
-
-  // constructor(){
-  //   super();
-  //   this.state = {txt: 'Front Side of Card'}
-  //   this.update = this.update.bind(this)
-  // }
-
-  // update( e ) {
-  //   this.setState({txt: 'Back Side of Card'})
-  // }
-
-  componentDidMount() {
-    // Axios is promise-based
-    axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
-      console.log(res)
-      this.setState({ cards: res.data });
-    })
+    cards : [
+      {name: 'Ryu', age: 30, belt: 'black', id: 1},
+      {name: 'Yoshi', age: 20, belt: 'green', id: 2},
+      {name: 'Crystal', age: 25, belt: 'pink', id: 3}
+    ]
   }
 
-  render(){
+  render() { // Added
     return (
-      <div className="flipcard-outer">
-        <div className="card-content">
-          <ul>
-              {this.state.cards.map(card => <li>{card.name}</li>)}
-          </ul>
-        </div>
+      <div className="App">
+        <h1>First React App</h1>
+        <Cards cards={ this.state.cards } />
+
+        {/* Acceptable way of outputting if not using a state, goin one at a time */}
+        {/* <Cards name="Ryu" age="25" belt="black" />  */}
       </div>
-      
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ react-flashcards/src/Components/Cards.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Cards extends Component {
+    componentDidMount() {
+        fetch('./data.json') // asynchronous, creates a promise
+        .then(res => { // Gets response from above call ^^
+          return res.json(); // Returns in json format
+        })
+        .then(data => {
+          console.log(data)
+        })
+    }
+    
+    render() {
+        // Below example coming from React series from The Net Ninja Youtube channel
+        // const cards = this.props;
+        const cardList = this.props.cards.map(card => { // List referencing the const above ^^
+            return (
+                <div className="card" key={ card.id } >
+                    {/* Must reference card from map cardList above to do a for each */}
+                    <div>Name: { card.name }</div>
+                    <div>Age: { card.age }</div>
+                    <div>Belt: { card.belt }</div>
+                </div>
+            )
+        })
+        // This method below called destructuring
+        // const { name, age, belt} = this.props; // Store name, age, and belt in variables called name, age, belt
+        return(
+            <div className="card-list">
+                { cardList }
+            </div>
+        //     // <div className="card">
+        //     //     {/* Below is WITH desstructuring from const above ^^ */}
+        //     //     {/* <div>Name: {name}</div>
+        //     //     <div>Age: {age}</div>
+        //     //     <div>Belt: {belt}</div> */}
+
+        //     //     {/* Below is an acceptable form of outputting data, but this is without destructuring */}
+        //     //     {/* <div>Name: {this.props.name}</div>
+        //     //     <div>Age: {this.props.age}</div>
+        //     //     <div>Belt: {this.props.belt}</div> */}
+        //     // </div>
+        )
+    }
+}
+
+export default Cards

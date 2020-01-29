d0709c8b142091499168b5733e801dbf79714bc1
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-01-29 15:18:25 -0600

    successfully pulling title from the local json call, then rendering

--- react-flashcards/src/Components/Cards.js
+++ react-flashcards/src/Components/Cards.js
@@ -11,52 +11,83 @@ class Cards extends Component {
     //     })
     // }
 
+    state = {
+        cards: []
+    }
+
     componentDidMount() {
+        let currentComponent = this; // Need to declare this before using inside of an axios get call
         const axios = require('axios');
         axios.get('http://localhost:3000/collections')
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function () {
-            console.log('error');
-        })
-        .then(function () {
-            console.log('test');
+        .then(function (res) {
+            console.log(res)
+            console.log(res.data)
+            console.log(res.data[2].title)
+            currentComponent.setState({
+                cards: res.data
+            })
         });
+        
     }
 
     render() {
-        // Below example coming from React series from The Net Ninja Youtube channel
-        // const cards = this.props;
-        const cardList = this.props.cards.map(card => { // List referencing the const above ^^
-            return (
-                <div className="card" key={ card.id } >
-                    {/* Must reference card from map cardList above to do a for each */}
-                    <div>Name: { card.name }</div>
-                    <div>Age: { card.age }</div>
-                    <div>Belt: { card.belt }</div>
+        const { cards } = this.state;
+        const cardList = cards.length ? (
+            cards.map(card => {
+                return (
+                <div className="card" key={card.id}>
+                    <div className="card-content">
+                        <span className="card-title">{card.title}</span>
+                    </div>
                 </div>
-            )
-        })
-        // This method below called destructuring
-        // const { name, age, belt} = this.props; // Store name, age, and belt in variables called name, age, belt
-        return(
-            <div className="card-list">
-                { cardList }
-            </div>
-        //     // <div className="card">
-        //     //     {/* Below is WITH desstructuring from const above ^^ */}
-        //     //     {/* <div>Name: {name}</div>
-        //     //     <div>Age: {age}</div>
-        //     //     <div>Belt: {belt}</div> */}
+                )
+            })
+            ) : (<div className="center">No info available</div> )
 
-        //     //     {/* Below is an acceptable form of outputting data, but this is without destructuring */}
-        //     //     {/* <div>Name: {this.props.name}</div>
-        //     //     <div>Age: {this.props.age}</div>
-        //     //     <div>Belt: {this.props.belt}</div> */}
-        //     // </div>
+        return (
+            <div className="card-container">
+                <div className="single-card">
+                    {cardList}
+                </div>
+            </div>
         )
     }
+
+
+    // render() {
+    //     // Below example coming from React series from The Net Ninja Youtube channel
+    //     // const cards = this.props;
+    //     const cardList = this.props.cards.map(card => { // List referencing the const above ^^
+    //         return (
+    //             <div className="card" key={ card.id } >
+    //                 {/* Must reference card from map cardList above to do a for each */}
+    //                 <div>Title: { card.title }</div>
+    //                 <div>Word: { card.cards.word }</div>
+    //                 <div>Description: { card.cards.definition }</div>
+    //             </div>
+    //         )
+    //     })
+    // }
+    //     // This method below called destructuring
+    //     // const { name, age, belt} = this.props; // Store name, age, and belt in variables called name, age, belt
+    //     return(
+    //         <div className="card-list">
+    //             { cardList }
+    //         </div>
+    //     );
+    //     //     // <div className="card">
+    //     //     //     {/* Below is WITH destructuring from const above ^^ */}
+    //     //     //     {/* <div>Name: {name}</div>
+    //     //     //     <div>Age: {age}</div>
+    //     //     //     <div>Belt: {belt}</div> */}
+
+    //     //     //     {/* Below is an acceptable form of outputting data, but this is without destructuring */}
+    //     //     //     {/* <div>Name: {this.props.name}</div>
+    //     //     //     <div>Age: {this.props.age}</div>
+    //     //     //     <div>Belt: {this.props.belt}</div> */}
+    //     //     // </div>
+    //     // )
+    // // }
 }
 
 export default Cards
